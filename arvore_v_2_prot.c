f41de2fe211cb88f2539f47172b04035f22e4a4d
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2022-09-19 16:20:17 -0300

    Update arvore_v_2_prot.c
    remoção de erros e atualização de funções para tentativa de implementação recursiva

--- arvore_v_2_prot.c
+++ arvore_v_2_prot.c
@@ -7,9 +7,9 @@ struct no {
     struct no *dir;
 };
 
-void inserir(struct no *raiz, Aluno dados);
-void inserir_esq(struct no *raiz, Aluno dados, struct no *novo);
-void inserir_dir(struct no *raiz, Aluno dados, struct no *novo);
+struct no *inserir(struct no *raiz, Aluno dados);
+struct no *inserir_esq(struct no *raiz, Aluno dados);
+struct no *inserir_dir(struct no *raiz, Aluno dados);
 /*
 typedef struct arvore{
     struct no *raiz;
@@ -26,43 +26,46 @@ struct no *raiz   = (struct no*)malloc(sizeof(struct no));
 
 struct no *NovoNo(Aluno a){
     struct no* novo = (struct no*)malloc(sizeof(struct no));
-           novo->dado = dados;
+           novo->dado = criaraluno2(a.nome, a.id, a.n1);
            novo->dir  = NULL;
            novo->esq  = NULL;
     return novo;
 }
 
-void inserir(struct no *raiz, Aluno dados){
+struct no *inserir(struct no *raiz, Aluno dados){
     if (raiz == NULL){
-    raiz = NovoNo(dados)
+    raiz = NovoNo(dados);
         }
     if(dados.id < raiz->dado.id){ //Primeira condição caso o conteudo inserido seja MENOR que o valor do conteudo da raiz. Neste caso, os novos dados são inseridos a esquerda da arvore
-       inserir_esq(raiz, dados);
+      raiz->esq = inserir_esq(raiz->esq, dados);
     }
         else if (dados.id > raiz->dado.id){ //Segunda condição caso o conteudo inserido seja MAIOR que o valor do conteudo da raiz. Neste caso, os novos dados são inseridos a direita da arvore
-       inserir_dir(raiz, dados); 
+       raiz->dir = inserir_dir(raiz->dir, dados); 
             }
-     
+     return raiz;
 }  
 
-void inserir_esq(struct no *raiz, Aluno dados){
+struct no *inserir_esq(struct no *raiz, Aluno dados){
     if(raiz->esq = NULL){
-            raiz->esq = dados;
+            raiz->esq->dado = dados;
         } else {
         struct no* pi;
         for(pi = raiz; pi->esq != NULL; pi = raiz->esq){
-            pi->esq = dados;
+            pi->esq->dado = dados;
         }
     }
+    return raiz;
 }
 
-void inserir_dir(struct no *raiz, Aluno dados){
+struct no *inserir_dir(struct no *raiz, Aluno dados){
     if(raiz->dir = NULL){ //Caso o primeiro galho da arvore aponte para um ponteiro vazio, o novo conteudo será inserido neste mesmo espaço
-            raiz->dir = dados;
+            raiz->dir->dado = dados;
         } else { //Caso contrario, andaremos pelos galhos até encontrar um espaço vazio onde será inserido os novos dados
                 struct no* pi;
         for(pi = raiz; pi->dir != NULL; pi = raiz->dir){
-            pi->dir = dados;
+            pi->dir->dado = dados;
         }
     }
+
+    return raiz;
 }
