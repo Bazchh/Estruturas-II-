2c26bd31183e416fc610dc1012b7eb99455b5d0c
Marcos Mikael Lima Vidal <90267184+Bazchh@users.noreply.github.com>
2022-09-22 09:14:37 -0300

    Update arvore_v_2_prot.c
    arquivo de inserção de dado sem arvore finalizado

--- arvore_v_2_prot.c
+++ arvore_v_2_prot.c
@@ -1,30 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "aluno.c"
+#include <locale.h>
 
 struct no {
     Aluno dado;
     struct no *esq;
     struct no *dir;
 };
-
+//Escopo das funções
 struct no *inserir(struct no *raiz, Aluno dados);
 struct no *inserir_esq(struct no *raiz, Aluno dados);
 struct no *inserir_dir(struct no *raiz, Aluno dados);
+void ler_arquivo(char arquivo[20], struct no *raiz);
+void preordem(struct no *raiz);
+void inordem(struct no *raiz);
+void posordem(struct no *raiz);
+void print(Aluno A);
+void print_tree(struct no *raiz, char ordem[9]);
+
 /*
 typedef struct arvore{
     struct no *raiz;
 }tree;
 */
 int main(){
+setlocale(LC_ALL,"");
 struct no *raiz   = (struct no*)malloc(sizeof(struct no));
        raiz->esq  = NULL;
        raiz->dir  = NULL;
       Aluno  *a = criaraluno("Mikael\0",72,7); //Insere a raiz
        raiz->dado = *a;
+       char aux[20];
+       int op;
+       printf("\nInsira o nome do arquivo e o tipo de arquivo a ser lido: ");
+       scanf("%s", aux);
+       ler_arquivo(aux,raiz);
 
+    printf("\nInsira em qual ordem a raiz deve ser impressa: ");
+    scanf("%s", aux);
+    print_tree(raiz,aux);
+
+    while(op!= 2){
+        printf("\nDeseja imprimir a raiz em outra ordem? ");
+        scanf("%i", &op);
+        if(op == 1){
+        printf("\nInsira em qual ordem a raiz deve ser impressa: ");
+        scanf("%s", aux);
+        print_tree(raiz, aux);
+        } 
+    }
 }
 
-struct no *NovoNo(Aluno a){
+struct no *novono(Aluno a){
     struct no* novo = (struct no*)malloc(sizeof(struct no));
            novo->dado = criaraluno2(a.nome, a.id, a.n1);
            novo->dir  = NULL;
@@ -34,38 +62,84 @@ struct no *NovoNo(Aluno a){
 
 struct no *inserir(struct no *raiz, Aluno dados){
     if (raiz == NULL){
-    raiz = NovoNo(dados);
+    raiz = novono(dados);
         }
-    if(dados.id < raiz->dado.id){ //Primeira condição caso o conteudo inserido seja MENOR que o valor do conteudo da raiz. Neste caso, os novos dados são inseridos a esquerda da arvore
-      raiz->esq = inserir_esq(raiz->esq, dados);
+    if(dados.id < raiz->dado.id){ //Primeira condição caso o conteudo inserido seja MEnOR que o valor do conteudo da raiz. Neste caso, os novos dados são inseridos a esquerda da arvore
+      raiz->esq = inserir(raiz->esq, dados);
     }
         else if (dados.id > raiz->dado.id){ //Segunda condição caso o conteudo inserido seja MAIOR que o valor do conteudo da raiz. Neste caso, os novos dados são inseridos a direita da arvore
-       raiz->dir = inserir_dir(raiz->dir, dados); 
+       raiz->dir = inserir(raiz->dir, dados); 
             }
      return raiz;
 }  
 
-struct no *inserir_esq(struct no *raiz, Aluno dados){
-    if(raiz->esq = NULL){
-            raiz->esq->dado = dados;
-        } else {
-        struct no* pi;
-        for(pi = raiz; pi->esq != NULL; pi = raiz->esq){
-            pi->esq->dado = dados;
-        }
-    }
-    return raiz;
+void ler_arquivo(char arquivo[20], struct no *raiz){
+FILE *arch = fopen(arquivo,"r");
+Aluno aux;
+
+if(arch == NULL){
+    printf("\n Não foi possivel abrir o arquivo");
 }
 
-struct no *inserir_dir(struct no *raiz, Aluno dados){
-    if(raiz->dir = NULL){ //Caso o primeiro galho da arvore aponte para um ponteiro vazio, o novo conteudo será inserido neste mesmo espaço
-            raiz->dir->dado = dados;
-        } else { //Caso contrario, andaremos pelos galhos até encontrar um espaço vazio onde será inserido os novos dados
-                struct no* pi;
-        for(pi = raiz; pi->dir != NULL; pi = raiz->dir){
-            pi->dir->dado = dados;
-        }
-    }
+while(!feof(arch)){
+    fscanf(arch,"%f %i %s", &aux.n1, &aux.n1,&aux.id,aux.nome);
+    inserir(raiz,aux);
+}
 
-    return raiz;
+fclose(arch);
+
+}
+void print(Aluno A) {
+    printf("Id: %d\nNome: %s\nNota: %f\n\n", A.id, A.nome, A.n1);
+}
+
+void preordem(struct no *raiz) {
+    if (raiz != 0) {
+        print(raiz->dado);
+    }
+    if (raiz->esq != 0) {
+        preordem(raiz->esq);
+    }
+    if (raiz->dir != 0) {
+        preordem(raiz->dir);
+    }
+}
+
+void inordem(struct no *raiz) {
+    
+    if (raiz->esq != 0) {
+        preordem(raiz->esq);
+    }
+    if (raiz != 0) {
+        print(raiz->dado);
+    
+    }
+    if (raiz->dir != 0) {
+        preordem(raiz->dir);
+        }
+}
+void posordem(struct no *raiz) {
+  
+    if (raiz->esq != 0) {
+        preordem(raiz->esq);
+
+    }
+    if (raiz->dir != 0) {
+        preordem(raiz->dir);
+    }  
+    if (raiz != 0) {
+        print(raiz->dado);
+    }
+}
+
+void print_tree(struct no *raiz, char ordem[9]){
+    if (ordem == "preordem"){
+        preordem(raiz);
+    } else if (ordem == "inordem"){
+        inordem(raiz);
+    } else if (ordem == "posordem"){
+        posordem(raiz);
+    } else {
+        printf("\nOpção escolhida é inválida");
+    }
 }
